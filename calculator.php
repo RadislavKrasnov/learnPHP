8bd409e88f7d5f315e3c9fbe008b61c42a802e73
RadislavKrasnov <smartty71@gmail.com>
2017-04-22 16:36:36 +0300

    My homework#2 I made calculator. There is two php files. formCalculator.php it's html structure (client side) and calculator.php,  that provide backend side.  if some cell is empty, will be return 'NULL' value. When divided by  zero will be return "E";

---
+++ calculator.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radik
+ * Date: 15.04.17
+ * Time: 16:53
+ */
+
+$symbol = $_POST['mathSymbol'];
+
+if (isset($_POST['num-1'], $_POST['num-2']) &&
+    !empty($_POST['num-1']) && !empty($_POST['num-2'])) {
+    $numFirst = (int)$_POST['num-1'];
+    $numSecond = (int)$_POST['num-2'];
+
+    switch ($symbol) {
+        case '+':
+            $result = $numFirst + $numSecond;
+            break;
+        case '-':
+            $result = $numFirst - $numSecond;
+            break;
+        case '*':
+            $result = $numFirst * $numSecond;
+            break;
+        case '/':
+            $result = $numFirst / $numSecond;
+            break;
+        case '**':
+            $result = pow($numFirst, $numSecond);
+            break;
+        case '%':
+            $result = $numFirst * ($numSecond / 100);
+            break;
+    }
+} elseif ( empty($_POST['num-2']) && $symbol == "/") {
+    $result = (string)'E';
+} elseif (empty($_POST['num-1']) || empty($_POST['num-2'])) {
+    $result = (string)'Null';
+}
+
+
+
+
+
+
